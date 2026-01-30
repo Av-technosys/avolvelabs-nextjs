7a61f20b5824b3d59c61ed8a068b5371ddec72f2
Lavesh06 <kumarlavesh2001@gmail.com>
2026-01-30 15:27:39 +0530

    loader

---
+++ src/components/ui/loader.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { motion } from "motion/react";
+import React from "react";
+
+
+
+export const LoaderFour = ({ text = "Avolvelabs..." }: { text?: string }) => {
+  return (
+    <div className=" relative font-bold text-[#032d60] [perspective:1000px] dark:text-white">
+      <motion.span
+        animate={{
+          rotateZ: [0, -20, 0],
+          scaleX: [1, 2, 1],
+        }}
+        transition={{
+          duration: 0.05,
+          repeat: Infinity,
+          repeatType: "reverse",
+          repeatDelay: 2,
+          ease: "linear",
+          times: [0, 0.2, 0.5, 0.8, 1],
+        }}
+        className="relative z-20 inline-block"
+      >
+        {text}
+      </motion.span>
+      <motion.span
+        className="absolute inset-0 text-[#032d60]/50 blur-[0.5px] dark:text-[#032d60]"
+        animate={{
+          x: [-2, 4, -3, 1.5, -2],
+          y: [-2, 4, -3, 1.5, -2],
+          opacity: [0.3, 0.9, 0.4, 0.8, 0.3],
+        }}
+        transition={{
+          duration: 0.5,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "linear",
+          times: [0, 0.2, 0.5, 0.8, 1],
+        }}
+      >
+        {text}
+      </motion.span>
+      <motion.span
+        className="absolute inset-0 text-[#8b00ff]/50 dark:text-[#8b00ff]"
+        animate={{
+          x: [0, 1, -1.5, 1.5, -1, 0],
+          y: [0, -1, 1.5, -0.5, 0],
+          opacity: [0.4, 0.8, 0.3, 0.9, 0.4],
+        }}
+        transition={{
+          duration: 0.8,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "linear",
+          times: [0, 0.3, 0.6, 0.8, 1],
+        }}
+      >
+        {text}
+      </motion.span>
+    </div>
+  );
+};
+
+export const LoaderFive = ({ text }: { text: string }) => {
+  return (
+    <div className="font-sans font-bold text-[#032d60] dark:[--shadow-color:var(--color-neutral-100)]">
+      {text.split("").map((char, i) => (
+        <motion.span
+          key={i}
+          className="inline-block"
+          initial={{ scale: 0, opacity: 0.5 }}
+          animate={{
+            scale: [1, 1.5, 1],
+            textShadow: [
+              "0 0 0 var(--shadow-color)",
+              "0 0 1px var(--shadow-color)",
+              "0 0 0 var(--shadow-color)",
+            ],
+            opacity: [0.5, 1, 0.5],
+          }}
+          transition={{
+            duration: 0.5,
+            repeat: Infinity,
+            repeatType: "loop",
+            delay: i * 0.05,
+            ease: "easeInOut",
+            repeatDelay: 2,
+          }}
+        >
+          {char === " " ? "\u00A0" : char}
+        </motion.span>
+      ))}
+    </div>
+  );
+};
